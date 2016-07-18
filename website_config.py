ad447702f5ef988cbb18a65e104704acf9e9c10a
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-07-18 15:49:02 -0700

    Removed unnecessary project files

--- website_config.py
+++
@@ -1,8 +0,0 @@
-import json
-
-SECRET_KEY = 'tMsCUhLizue4cIfAe8s6vkbOWGBB9wUF99N5J0Whbtlf8a6iKIC3R30Rdbo4y6B'
-DEBUG = False
-CLIENT_SECRETS = 'client_secrets.json'
-CLIENT_ID = json.loads(open(CLIENT_SECRETS, 'r').read())['web']['client_id']
-
-IMGUR_CLIENT_ID = '873ad6bb61b29ec'
